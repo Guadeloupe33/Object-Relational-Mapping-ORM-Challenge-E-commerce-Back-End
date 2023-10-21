6e77740dace9d838e06201af8a99d6498c0d8a18
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-20 20:46:25 -0400

    added api routes to the project

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,6 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+Product.belongsTo(Category);
+
+Category.hasMany(Product);
+
+Product.belongsToMany(Tag, {
+  through: { 
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+});
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+});
+
 // Products belongsTo Category
 
 // Categories have many Products

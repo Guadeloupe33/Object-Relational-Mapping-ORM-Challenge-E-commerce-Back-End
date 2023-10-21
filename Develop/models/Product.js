9b973e3af0a470648583c2b9171cc6695b7b0ca7
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-20 20:52:52 -0400

    added product columns to the project

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,43 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+
+id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: "category",
+        key: "id"
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 15,
+      validate: {
+        isNumeric: true
+      }
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+
   },
   {
     sequelize,
